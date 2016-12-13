2a30ec44ddad87c133a27dca27a0bb07e67af768
zhaobe <zhaoben777@gmail.com>
2016-12-13 11:37:34 -0800

    Added the facebook fox with balloons image locally and referenced it.

--- AwesomeProject/index.ios.js
+++ AwesomeProject/index.ios.js
@@ -4,59 +4,56 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, Image, StyleSheet, Text, View} from 'react-native';
 
 export default class AwesomeProject extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-            OR
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Welcome to React Native!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.ios.js
+                </Text>
+                <Text style={styles.instructions}>
+                    OR
+                </Text>
+                <Text style={styles.instructions}>
+                    Press Cmd+R to reload,{'\n'}
+                    Cmd+D or shake for dev menu
+                    {"\n"}
+                </Text>
+                <Image source={require('./img/react-native-congratulations.png')} style={{width: 200, height: 200}}/>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    margin: 10,
-    padding: 10,
-    borderColor: 'blue',
-    borderWidth: 1,
-    borderRadius: 4,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#000000',
-    marginBottom: 5,
-    fontWeight: 'bold',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: 10,
+        padding: 10,
+        borderColor: 'blue',
+        borderWidth: 1,
+        borderRadius: 4
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#000000',
+        marginBottom: 5,
+        fontWeight: 'bold'
+    },
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Getting Started with React Native
 
 - Downloaded the react-native 0.38.0 version with ```react-native init AwesomeProject --version 0.38.0```
-- To run, open AwesomeProject.xcodeproj in XCode and run ios simulator to render app. Had to comment out line 26-27 in React > Base > RCTPlatform.c > interfaceIdiom(), since there was a build fail for the UIUserInterfaceIdiomCarPlay. 
+- To run, open AwesomeProject.xcodeproj in XCode and run ios simulator to render app. Had to comment out line 26-27 in React > Base > RCTPlatform.c > interfaceIdiom(), since there was a build fail for the UIUserInterfaceIdiomCarPlay.
 - Had to clear cache with ```npm start -- --reset-cache``` in order to get the ios simulator to be error-free.
 
-#Basic Syntax
+# Basic Syntax
 - Components can be made with certain parameters called ```props```. Which can allow one single component to be used in different places.
--
+
+# Small Fixes
+- Will need to restart the simulator when adding a new image to a local directory in the project.
